eae2cae5cacf6a02c4554298aec5bb7210356838
suochan <suochantsao@gmail.com>
2021-03-17 11:48:35 +0800

    建立環境

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <index-banner msg="White Space"/>
   </div>
+
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import indexBanner from './components/banner.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    indexBanner
   }
 }
+
 </script>
 
+
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+  border: 1px solid pink;
 }
+
 </style>
---
+++ src/components/banner.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="bgImg">
+    <h1 class="logo">
+      <img src="../assets/icon/logo_white.svg" alt="WhiteSpaceLogo">
+    </h1>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name: 'indexBanner',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.bgImg{
+  background-image: url('https://images.unsplash.com/photo-1519974719765-e6559eac2575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+  background-size: cover;
+  background-position: center;
+  
+}
+
+</style>
